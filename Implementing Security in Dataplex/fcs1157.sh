b8f9b0351c744d89d3d1446afc241dae83e95a02
FutureCloudSkill <sahashubhadeep83@gmail.com>
2026-01-21 13:59:16 +0530

    Update fcs1157.sh

--- Implementing Security in Dataplex/fcs1157.sh
+++ Implementing Security in Dataplex/fcs1157.sh
@@ -16,5 +16,14 @@ gcloud dataplex assets create customer-online-sessions --location=$REGION \
             --resource-type=STORAGE_BUCKET \
             --resource-name=projects/$DEVSHELL_PROJECT_ID/buckets/$DEVSHELL_PROJECT_ID-bucket \
             --display-name="Customer Online Sessions"
+
+
+echo "${CYAN}${BOLD}Click here: "${RESET}""${BLUE}${BOLD}"https://console.cloud.google.com/dataplex/secure?resourceName=projects%2F$DEVSHELL_PROJECT_ID%2Flocations%2F$REGION%2Flakes%2Fcustomer-info-lake&project=$DEVSHELL_PROJECT_ID""${RESET}"
+
             
 echo "✅ Script execution completed"
+
+
+echo
+echo "${GREEN_TEXT}${BOLD_TEXT}${UNDERLINE_TEXT}https://www.youtube.com/@Future Cloud Skill${RESET_FORMAT}"
+echo
